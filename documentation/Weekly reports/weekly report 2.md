8aa4d8a934eec5568436cd47669063c5696908a6
xavier <x@xavier.dev>
2024-03-23 21:49:31 +0200

    Starting working on the weekly report 2 and created docstrings for dijkstra.py

---
+++ documentation/Weekly reports/weekly report 2.md
@@ -0,0 +1,15 @@
+# Weekly Report 2 
+1. What have I been up to this week? <br />
+   - [x] I have read the paper about JPS. I looked into slides of Datastructures and Algorithms and also at my old implementation of **Dijkstra** in `Python`. Although I have written Dijkstra in many forms before, never did I specifically create a path finding algorithm for a bitmaps with uniform difficulty and obstacles. I decided to write it based on the slides and my previous implementation, and also allow selecting a concrete map for testing the algorithms. I separated the logic of command line use of the tool and the actual algorithm. I wrote tests for Dijkstra.<br />
+2. How has the program progressed? <br />
+   - [x] I'm happy that implementing **Djikstra** went so smoothfully, and that controlling the tool from the command line is quite convenient. I faced issues with **JPS** since it's still pretty foreign to me and the algorithm in the paper is pretty high level.<br />
+3. What did I learn this week / today? <br />
+   - [x] I learned to use `coverage` to keep track of whether my algorithm works well. I learnt to write my own tests with `unittest`. I learned what maps look like in practice and how to convert them into graphs needed by Dijkstra <br />
+4. What was unclear or caused difficulties? Answer this section honestly, as you will receive help based on this section if needed.  <br />
+   - [x] I wasn't sure how much of my solution should be purely original. I reworked my earlier **Dijkstra** algorithm and worked with pseudocode from the lecture slides, but I might as well have just adopted the [geeksforgeeks](https://www.geeksforgeeks.org/python-program-for-dijkstras-shortest-path-algorithm-greedy-algo-7/) solution, which I didn't do after all. I suppose the **JPS** also causes problems for me because I need to somehow implement the *pruning* rules on my own. <br />
+4. What do I do next? <br />
+   - [x] I need to include better comments in my code. I need to get coverage up to 100% if I haven't done it yet. I need to work on **JPS** and the documentation. I need to create a nice visualization of the progress of both algorithms, which I plan to do in the command line. <br /><br />
+
+**Other considerations:** <br />
+I spent around **8** hours this week working on the project. <br />
+ 
--- src/dijkstra.py
+++ src/dijkstra.py
@@ -1,11 +1,34 @@
 from heapq import * 
     
 class Edge:
+    """A class with which we can keep track of what edges and with what weights are the neighbours of a node
+
+    Attributes:
+        end: the terminating edge
+        weight: the weight or cost to access edge
+    """
+
     def __init__(self, v, weight):
         self.end = v
         self.weight = weight
+        """Class constructor, that creates a new edge
+        
+        Args:
+            end: the terminating edge
+            weight: the weight or cost to access edge
+        """
 
 class Dijkstra:
+    """A class that can turn arrays describing a map into a graph and find the shortest path using Dijkstra algorithm
+    
+    Attributes:
+        map: an array of rows, each describing a row of squares, where each square is an obstacle or is free
+        number_of_nodes: number of nodes in total equal to number of squares
+        adjacencylist: an array of arrays, one for each square/node
+        num_rows: number of rows of the map
+        len_row: length of a single row
+    """
+
     def __init__(self,map):
         self.map = map 
         self.number_of_nodes = len(map)*len(map[0])
@@ -15,16 +38,45 @@ class Dijkstra:
         self.create_edges_from_map()
 
     def __str__(self):
-        return f'Number of nodes: {self.number_of_nodes}'
+        """A function used to test if Dijkstra class correctly found the number of nodes
+        
+        Returns:
+            A string describing the number of nodes 
+        """
 
+        return f'Number of nodes: {self.number_of_nodes}'
+        
     def add_edge(self,a,b,x):
+        """A function used to add an edge between two nodes
+
+        Args:
+            a: node on one side
+            b: node on the other side
+            x: the weight/cost of the edge
+        """
+
         self.adjacencylist[a].append(Edge(b,x))
         self.adjacencylist[b].append(Edge(a,x))
 
     def edge_number(self, i,j):
+        """A handy function for converting a position of a square on the map into a node
+    
+        Args:
+            i: row of the square
+            j: column (or index in the row) of the square
+
+        Returns:
+            A node number
+        """
+
         return i*self.len_row+j
 
     def create_edges_from_map(self):
+        """A function that turns an array of rows into a graph that can be used by Dijkstra
+        
+        Is only run once during the initialization of the class
+        """
+
         nrows = self.num_rows
         for i in range(len(self.map)):
             nrows=nrows-1
@@ -42,6 +94,16 @@ class Dijkstra:
                             self.add_edge(self.edge_number(i,j),self.edge_number(i+1,j+1),1.41)
 
     def find_shortest_path(self,a,b):
+        """Actual implementation of the Dijkstra algorithm
+
+        Args:
+            a: starting node
+            b: goal node
+
+        Returns:
+            The shortest path to 2 decimal digits
+        """
+
         processed = [False] * self.number_of_nodes
         distance = [10000] * self.number_of_nodes
         distance[a]=0

c6e7d4824401f29c6087ece7306f4472585ec12b
xavier <x@xavier.dev>
2024-04-04 02:57:07 +0200

    Added invoke tasks

---
+++ tasks.py
@@ -0,0 +1,41 @@
+import os 
+from invoke import task
+from subprocess import call
+from sys import platform
+
+root_dir_raw = os.path.dirname(os.path.abspath(__file__))
+root_dir = "'"+root_dir_raw+"'"
+
+
+@task
+def foo(ctx):
+    print("bar")
+'''Doesn't work yet
+@task
+def start(ctx, args):
+    args = args.strip()
+    print(args)
+    ctx.run(f"python3 {root_dir}/src/main.py "+args, pty=True)
+'''    
+@task
+def jps(ctx):
+    ctx.run(f"cd {root_dir}/src && python3 main.py --jps --map wall.map 0 0 4 7", pty=True)
+
+@task
+def dijkstra(ctx):
+    ctx.run(f"cd {root_dir}/src && python3 main.py --dijkstra --map arena.map 4 3 5 11", pty=True)
+
+@task
+def test(ctx):
+    ctx.run(f"cd {root_dir} && pytest {root_dir}/src", pty=True)
+
+
+@task
+def coverage(ctx):
+    ctx.run(f"cd {root_dir} && coverage run --branch -m pytest src", pty=True)
+
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run(f"cd {root_dir} && coverage html", pty=True)
+    if platform != "win32":
+        call(("xdg-open", f"{root_dir_raw}/htmlcov/index.html"))

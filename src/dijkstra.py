d683aa01354a923d80dcfb23a2076279d580b81b
xavier <x@xavier.dev>
2024-04-10 17:22:53 +0200

    Added Presentation class

--- src/dijkstra.py
+++ src/dijkstra.py
@@ -1,4 +1,6 @@
+
 from heapq import *
+
 
 class Presentation:
     def __init__(self, map):
@@ -6,37 +8,27 @@ class Presentation:
         self.num_rows = len(self.map)
         self.len_row = len(self.map[0])
         self.slides = []
-            
+
     def __str__(self):
-        return self.map
+        return str(self.map)
 
     def mark(self, coordinates):
-        i,j=coordinates
-        self.map[i][j]="A"
+        i, j = coordinates
+        self.map[i][j] = "A"
 
     def print_map(self):
         rotated_map_list = [[''] * self.num_rows for _ in range(self.len_row)]
+        rotated_regular_map = [[''] * self.num_rows for _ in range(self.len_row)]
 
-        rotated_regular_map = [
-            [''] * self.num_rows for _ in range(self.len_row)]
-
-        rmap = ""
         for i in range(self.num_rows):
             for j in range(self.len_row):
                 rotated_regular_map[self.len_row - j - 1][i] = self.map[i][j]
 
+        rmap = ""
         for row in rotated_regular_map:
-            rmap = rmap+(" ".join(row))+"\n"
+            rmap = rmap + (" ".join(row)) + "\n"
 
-        #print(map)
-        #self.slides.append(rmap)
-        #if self.visual:
-        #    time.sleep(self.wait_time)
         return rmap
-
-
-
-
 
 
 class Edge:
@@ -47,15 +39,9 @@ class Edge:
         weight: the weight or cost to access edge
     """
 
-    def __init__(self, v, weight):
-        self.end = v
+    def __init__(self, coordinates, weight):
+        self.end = coordinates
         self.weight = weight
-        """Class constructor, that creates a new edge
-        
-        Args:
-            end: the terminating edge
-            weight: the weight or cost to access edge
-        """
 
 
 class Dijkstra:
@@ -70,10 +56,9 @@ class Dijkstra:
     """
 
     def __init__(self, map):
-        self.presentation = Presentation(map) 
-        self.map = map
+        self.presentation = Presentation(map)
         self.map = [list(row) for row in map]
-        self.number_of_nodes = len(map)*len(map[0])
+        self.number_of_nodes = len(map) * len(map[0])
         self.adjacencylist = [[] for _ in range(self.number_of_nodes)]
         self.num_rows = len(self.map)
         self.len_row = len(self.map[0])
@@ -101,33 +86,18 @@ class Dijkstra:
         self.adjacencylist[a].append(Edge(b, x))
         self.adjacencylist[b].append(Edge(a, x))
 
-    def edge_number(self, i, j):
-        """A handy function for converting a position of a square on the map into a node number
+    def coordinates_to_node(self, coordinates):
+        """A handy function for converting coordinates into a node number
 
         Args:
-            i: row of the square
-            j: column (or index in the row) of the square
+            coordinates: tuple of (i, j) representing row and column indices
 
         Returns:
             A node number
         """
 
-        return i*self.len_row+j
-
-    def coordinates(self, node_number):
-        """A handy function for converting a node number into a position of a square on the map
-
-        Args:
-            node_number, used to uniquely identify a field
-
-        Returns:
-            A tuple of (i,j), where
-                i: row of the square
-                j: column (or index in the row) of the square
-        """
-
-        return i*self.len_row+j
-
+        i, j = coordinates
+        return i * self.len_row + j
 
     def create_edges_from_map(self):
         """A function that turns an array of rows into a graph that can be used by Dijkstra
@@ -140,36 +110,36 @@ class Dijkstra:
         map = self.presentation.map
 
         for i in range(nrows):
-            nrows = nrows-1
+            nrows = nrows - 1
             for j in range(lrow):
                 if map[i][j] == "T":
                     continue
                 else:
                     if j < lrow:
-                        if map[i][j+1] == ".":
-                            self.add_edge(self.edge_number(i, j),
-                                          self.edge_number(i, j+1), 1)
+                        if map[i][j + 1] == ".":
+                            self.add_edge(self.coordinates_to_node((i, j)),
+                                          self.coordinates_to_node((i, j + 1)), 1)
                     if nrows != 0:
-                        if map[i+1][j] == ".":
-                            self.add_edge(self.edge_number(i, j),
-                                          self.edge_number(i+1, j), 1)
-                        if map[i+1][j+1] == ".":
-                            self.add_edge(self.edge_number(i, j),
-                                          self.edge_number(i+1, j+1), 1.41)
+                        if map[i + 1][j] == ".":
+                            self.add_edge(self.coordinates_to_node((i, j)),
+                                          self.coordinates_to_node((i + 1, j)), 1)
+                        if map[i + 1][j + 1] == ".":
+                            self.add_edge(self.coordinates_to_node((i, j)),
+                                          self.coordinates_to_node((i + 1, j + 1)), 1.41)
 
     def find_shortest_path(self, start_coordinates, end_coordinates, slides=False):
         """Actual implementation of the Dijkstra algorithm
 
         Args:
-            a: starting node
-            b: goal node
+            start_coordinates: starting coordinates (i, j)
+            end_coordinates: goal coordinates (i, j)
 
         Returns:
             The shortest path to 2 decimal digits
         """
 
-        a = self.edge_number(*start_coordinates)
-        b = self.edge_number(*end_coordinates)
+        a = self.coordinates_to_node(start_coordinates)
+        b = self.coordinates_to_node(end_coordinates)
         processed = [False] * self.number_of_nodes
         distance = [10000] * self.number_of_nodes
         distance[a] = 0

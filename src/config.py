06e6c251008b113f85d4364b52dad0f1d7fad566
xavier <x@xavier.dev>
2024-04-11 15:58:44 +0200

    Added config for easier path finding:

---
+++ src/config.py
@@ -0,0 +1,10 @@
+import os
+
+current_directory = os.getcwd()
+root_directory = current_directory#os.path.dirname(current_directory)
+maps_directory = os.path.join(root_directory, "maps")
+default_dijkstra = os.path.join(maps_directory, "arena.map")
+default_jps = os.path.join(maps_directory, "wall.map")
+
+if __name__=="__main__":
+    print("Current Directory:", current_directory)

f79f4c2f142a6ce77246668ad1c88f102ef195a2
xavier <x@xavier.dev>
2024-04-11 16:00:07 +0200

    Fixed bad map format:

--- src/dijkstra.py
+++ src/dijkstra.py
@@ -1,5 +1,5 @@
-
 from heapq import *
+from copy import deepcopy
 
 
 class Presentation:
@@ -12,9 +12,13 @@ class Presentation:
     def __str__(self):
         return str(self.map)
 
-    def mark(self, coordinates):
+    def mark(self, coordinates, mark):
         i, j = coordinates
-        self.map[i][j] = "A"
+        self.map[i][j] = mark
+
+    def add_slide(self):
+        #self.slides.append(deepcopy(self.map))
+        self.slides.append(self.print_map())
 
     def print_map(self):
         rotated_map_list = [[''] * self.num_rows for _ in range(self.len_row)]
@@ -30,7 +34,6 @@ class Presentation:
 
         return rmap
 
-
 class Edge:
     """A class with which we can keep track of what edges and with what weights are the neighbours of a node
 
@@ -39,10 +42,15 @@ class Edge:
         weight: the weight or cost to access edge
     """
 
-    def __init__(self, coordinates, weight):
-        self.end = coordinates
+    def __init__(self, v, weight):
+        self.end = v
         self.weight = weight
-
+        """Class constructor, that creates a new edge
+        
+        Args:
+            end: the terminating edge
+            weight: the weight or cost to access edge
+        """
 
 class Dijkstra:
     """A class that can turn arrays describing a map into a graph and find the shortest path using Dijkstra algorithm
@@ -55,15 +63,14 @@ class Dijkstra:
         len_row: length of a single row
     """
 
-    def __init__(self, map):
-        self.presentation = Presentation(map)
-        self.map = [list(row) for row in map]
-        self.number_of_nodes = len(map) * len(map[0])
+    def __init__(self, map, presentation):
+        self.presentation = presentation
+        self.map = map
+        self.number_of_nodes = len(map)*len(map[0])
         self.adjacencylist = [[] for _ in range(self.number_of_nodes)]
         self.num_rows = len(self.map)
         self.len_row = len(self.map[0])
         self.create_edges_from_map()
-        self.slides = False
 
     def __str__(self):
         """A function used to test if Dijkstra class correctly found the number of nodes
@@ -86,60 +93,74 @@ class Dijkstra:
         self.adjacencylist[a].append(Edge(b, x))
         self.adjacencylist[b].append(Edge(a, x))
 
-    def coordinates_to_node(self, coordinates):
-        """A handy function for converting coordinates into a node number
+    def edge_number(self, i, j):
+        """A handy function for converting a position of a square on the map into a node number
 
         Args:
-            coordinates: tuple of (i, j) representing row and column indices
+            i: row of the square
+            j: column (or index in the row) of the square
 
         Returns:
             A node number
         """
 
-        i, j = coordinates
-        return i * self.len_row + j
+        return i*self.len_row+j
+
+    def coordinates(self, node_number):
+        """A handy function for converting a node number into a position of a square on the map
+
+        Args:
+            node_number, used to uniquely identify a field
+
+        Returns:
+            A tuple of (i,j), where
+                i: row of the square
+                j: column (or index in the row) of the square
+        """
+        i = node_number //self.len_row
+        j = node_number%self.len_row
+        return (i,j)
+
 
     def create_edges_from_map(self):
         """A function that turns an array of rows into a graph that can be used by Dijkstra
 
-        It is only run once during the initialization of the class
+        Is only run once during the initialization of the class
         """
 
         nrows = self.num_rows
-        lrow = self.len_row
-        map = self.presentation.map
-
-        for i in range(nrows):
-            nrows = nrows - 1
-            for j in range(lrow):
-                if map[i][j] == "T":
+        for i in range(len(self.map)):
+            nrows = nrows-1
+            for j in range(self.len_row):
+                if self.map[i][j] == "T":
                     continue
                 else:
-                    if j < lrow:
-                        if map[i][j + 1] == ".":
-                            self.add_edge(self.coordinates_to_node((i, j)),
-                                          self.coordinates_to_node((i, j + 1)), 1)
-                    if nrows != 0:
-                        if map[i + 1][j] == ".":
-                            self.add_edge(self.coordinates_to_node((i, j)),
-                                          self.coordinates_to_node((i + 1, j)), 1)
-                        if map[i + 1][j + 1] == ".":
-                            self.add_edge(self.coordinates_to_node((i, j)),
-                                          self.coordinates_to_node((i + 1, j + 1)), 1.41)
+                    if j < self.len_row:
+                        if self.map[i][j+1] == ".":
+                            self.add_edge(self.edge_number(i, j),
+                                          self.edge_number(i, j+1), 1)
+                    if self.num_rows != 0:
+                        if self.map[i+1][j] == ".":
+                            self.add_edge(self.edge_number(i, j),
+                                          self.edge_number(i+1, j), 1)
+                        if self.map[i+1][j+1] == ".":
+                            self.add_edge(self.edge_number(i, j),
+                                          self.edge_number(i+1, j+1), 1.41)
 
-    def find_shortest_path(self, start_coordinates, end_coordinates, slides=False):
+    def find_shortest_path(self, start_coordinates, end_coordinates):
         """Actual implementation of the Dijkstra algorithm
 
         Args:
-            start_coordinates: starting coordinates (i, j)
-            end_coordinates: goal coordinates (i, j)
+            a: starting node
+            b: goal node
 
         Returns:
             The shortest path to 2 decimal digits
         """
+        map = self.presentation
 
-        a = self.coordinates_to_node(start_coordinates)
-        b = self.coordinates_to_node(end_coordinates)
+        a = self.edge_number(*start_coordinates)
+        b = self.edge_number(*end_coordinates)
         processed = [False] * self.number_of_nodes
         distance = [10000] * self.number_of_nodes
         distance[a] = 0
@@ -151,6 +172,8 @@ class Dijkstra:
             u = next[1]
             if not processed[u]:
                 processed[u] = True
+                map.mark(self.coordinates(u), "V")
+                map.add_slide()
                 for edge in self.adjacencylist[u]:
                     v = edge.end
                     if distance[v] > distance[u] + edge.weight:
@@ -159,4 +182,8 @@ class Dijkstra:
 
         if distance[b] == 10000:
             distance[b] = -1
+        print(map.slides[1000])
+        deep = deepcopy(map.slides[1000])
+        print(deep)
+
         return round(distance[b], 2)

570b17d98cac6fcd7d9ae91186ff2d9edf42c5ce
xavier <x@xavier.dev>
2024-04-20 14:19:45 +0200

    Added correctness tests:

--- src/tests/dijkstra_test.py
+++ src/tests/dijkstra_test.py
@@ -7,10 +7,11 @@ class TestDijkstra(unittest.TestCase):
     def setUp(self):
         print("Set up goes here")
         # path = "../../maps/arena.map"
-        path = "maps/arena.map"
-        lines = create_map(path)
+        self.arena_map = create_map("maps/arena.map")
+        self.arena2_map = create_map("maps/arena2.map")
+
         slides = []
-        self.algorithm = Dijkstra(lines)
+        self.algorithm = Dijkstra(self.arena_map)
         # a = args.start_node
         # b = args.end_node
 
@@ -21,6 +22,27 @@ class TestDijkstra(unittest.TestCase):
         slides = []
         distance = self.algorithm.find_shortest_path((4, 3), (5, 11), slides, visual=False)
         self.assertEqual(distance, 8.41)
+
+    def test_dijkstra_finds_straight_distance_on_arena_map(self):
+        slides = []
+        distance = self.algorithm.find_shortest_path((3, 43), (27, 43), slides, visual=False)
+        self.assertEqual(distance, 24)
+
+    def test_dijkstra_finds_distance_on_arena_map_1(self):
+        slides = []
+        distance = self.algorithm.find_shortest_path((5, 5), (20, 20), slides, visual=False)
+        self.assertAlmostEqual(float(distance), 23.5, places=1)
+
+    def test_dijkstra_finds_distance_on_arena_map_2(self):
+        slides = []
+        distance = self.algorithm.find_shortest_path((2, 2), (40, 40), slides, visual=False)
+        self.assertAlmostEqual(float(distance), 55.9, places=1)
+
+    def test_dijkstra_finds_distance_on_arena_map_2(self):
+        algorithm = Dijkstra(self.arena2_map)
+        distance = algorithm.find_shortest_path((1, 2), (5,20))
+        self.assertGreaterEqual(float(distance), 21)
+        self.assertLessEqual(float(distance), 22)
 
 
 '''

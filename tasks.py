91de08a46719dc9273af91f4ee899851e78db4ae
xavier <x@xavier.dev>
2024-04-27 20:05:57 +0200

    Added lint task and deleted outdated dijkstra and jps tasks:

--- tasks.py
+++ tasks.py
@@ -11,14 +11,6 @@ def start(ctx):
     ctx.run(f"python3 {root_dir}/src/main.py ", pty=True)
 
 @task
-def jps(ctx):
-    ctx.run(f"cd {root_dir}/src && python3 cli.py --jps --visual --map wall.map 0 0 4 7", pty=True)
-
-@task
-def dijkstra(ctx):
-    ctx.run(f"cd {root_dir}/src && python3 cli.py --dijkstra --map arena.map 4 3 5 11", pty=True)
-
-@task
 def test(ctx):
     ctx.run(f"cd {root_dir} && pytest {root_dir}/src", pty=True)
 
@@ -31,6 +23,10 @@ def coverage_report(ctx):
     ctx.run(f"cd {root_dir} && coverage html", pty=True)
     if platform != "win32":
         call(("xdg-open", f"{root_dir_raw}/htmlcov/index.html"))
+
+@task
+def lint(ctx):
+    ctx.run(f"cd {root_dir} && pylint src", pty=True)
 
 @task
 def format(ctx):

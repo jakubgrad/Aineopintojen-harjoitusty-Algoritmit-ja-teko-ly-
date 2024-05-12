9f6f64b41ab49768d97c96b6c10974f24dfdb179
xavier <x@xavier.dev>
2024-05-13 00:55:42 +0200

    Added encoding, eliminated unnecessary variables:

--- src/create_map.py
+++ src/create_map.py
@@ -1,5 +1,6 @@
 def create_map(path):
-    """A handy function that takes a map from the folder maps in the root directory and returns it as a list of list 
+    """A handy function that takes a map from the folder maps
+       in the root directory and returns it as a list of list 
 
     Args:
         Path to the map, e.g. map/arena.map in the form ...TTT..T etc.
@@ -9,16 +10,14 @@ def create_map(path):
         ....where T is an obstacle, . is a free square
     """
 
-    with open(path, "r") as file:
+    with open(path, "r", encoding='utf-8') as file:
         lines = []
         file.readline()
-        height_line = file.readline().strip()
-        height = height_line.split(" ")[1]
-        width_line = file.readline().strip()
-        width = width_line.split(" ")[1]
+        _ = file.readline().strip()
+        _ = file.readline().strip()
         file.readline()
         for l in file:
             line = l.strip()
             lines.append(line)
-    map = [list(row) for row in lines]
-    return map
+    grid = [list(row) for row in lines]
+    return grid

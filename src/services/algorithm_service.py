d90c7d588e37f793a273b625f685db2450a23f5c
xavier <x@xavier.dev>
2024-05-13 00:56:38 +0200

    Removed unnecessary information, functions, and linted the code:

--- src/services/algorithm_service.py
+++ src/services/algorithm_service.py
@@ -1,15 +1,15 @@
+import time
 from jps import JPS
-from math import log
 from dijkstra import Dijkstra
 from create_map import create_map
-import time
 
 
 class AlgorithmService():
     def __init__(self):
         pass
 
-    def run_algorithm(self, start_coordinates, goal_coordinates, map_path, slides, jps=False, dijkstra=False, visual=False):
+    def run_algorithm(self, start_coordinates, goal_coordinates,
+            map_path, slides, jps=False, visual=False):
         """Initializes and runs selected algorithm to find shortest path 
         between the start and goal coordinates on specified map.
 
@@ -25,16 +25,18 @@ class AlgorithmService():
         Returns:
             distance (float): The shortest distance between the start and goal points.
             execution_time (float): The initialization and running time of the algorithm in seconds.
-            message (str): A message containing information about the algorithm's results and performance.
+            message (str): A message containing information about the algorithm's results
+                and performance.
         """
 
         start_coordinates = self.coordinates_extract(start_coordinates)
         goal_coordinates = self.coordinates_extract(goal_coordinates)
-        map = create_map(map_path)
+        grid = create_map(map_path)
 
         start_time = time.time()
 
-        algorithm = JPS(map) if jps else Dijkstra(map)
+        algorithm = JPS(grid) if jps else Dijkstra(grid)
+
         distance = algorithm.find_shortest_path(
             start_coordinates, goal_coordinates, slides, visual=visual)
 
@@ -42,34 +44,12 @@ class AlgorithmService():
 
         execution_time = end_time - start_time
 
-        n_of_vertices = self.count_vertices(map_path)
-
-        jps_branching_factor = 3 
-
-        dijkstra_time_complexity = distance ** 2
         #jps_time_complexity = jps_branching_factor**distance
 
         message = (f"Distance is {distance}\n"
-                   f"Execution time is {execution_time}\n"
-                   f"Number of vertices is {n_of_vertices}\n"
-                   f"JPS branching factor assumed to be {jps_branching_factor}\n"
-                   f"Dijkstra time complexity of O(d^2) = {dijkstra_time_complexity}\n")
+                   f"Execution time is {execution_time}\n")
 
         return distance, execution_time, message
-
-    def count_vertices(self, map_path):
-        """Count the number of vertices in the map represented by the given map path.
-
-        Args:
-            map_path (str): The path to the map file.
-
-        Returns:
-            The total number of vertices in the map.
-        """
-
-        map = create_map(map_path)
-        algorithm = Dijkstra(map)
-        return len(algorithm.vertices)
 
     def coordinates_ok(self, start, goal, map_path):
         """Check if start and goal coordinates are in a valid format.
@@ -84,15 +64,15 @@ class AlgorithmService():
             False otherwise.
         """
 
-        map = create_map(map_path)
+        grid = create_map(map_path)
 
         start = self.coordinates_extract(start)
         goal = self.coordinates_extract(goal)
-        if start == False or goal == False:
+        if start is False or goal is False:
             return False
 
-        rows = len(map)
-        cols = len(map[0])
+        rows = len(grid)
+        cols = len(grid[0])
 
         x, y = start
         if not (0 <= x < rows and 0 <= y < cols):
@@ -102,15 +82,6 @@ class AlgorithmService():
         if not (0 <= x < rows and 0 <= y < cols):
             return False
 
-        return True
-
-    def test_coordinates(self, start, goal):
-        start = self.coordinates_extract(start)
-        goal = self.coordinates_extract(goal)
-        if start == False or goal == False:
-            return False
-        if not self.is_within_map(start) or not self.is_within_map(goal):
-            return False
         return True
 
     def view_map(self, map_path):
@@ -124,18 +95,18 @@ class AlgorithmService():
             flipped so that users can use cartesian coordinates.
         """
 
-        map = create_map(map_path)
-        num_rows = len(map)
-        len_row = len(map[0])
+        grid = create_map(map_path)
+        num_rows = len(grid)
+        len_row = len(grid[0])
 
-        rotated_map = [[''] * num_rows for _ in range(len_row)]
+        rotated_grid = [[''] * num_rows for _ in range(len_row)]
 
         for i in range(num_rows):
             for j in range(len_row):
-                rotated_map[len_row - j - 1][i] = map[i][j]
+                rotated_grid[len_row - j - 1][i] = grid[i][j]
 
         preview = ""
-        for i, row in enumerate(rotated_map):
+        for i, row in enumerate(rotated_grid):
             row_with_numbers = [str(len_row - i - 1)] + row
             preview += " ".join(row_with_numbers) + "\n"
         last_row = " ".join(str(i) for i in range(num_rows))
@@ -163,7 +134,7 @@ class AlgorithmService():
                 raise ValueError("Both coordinates must be integers")
             return (x, y)
         except ValueError as e:
-            messagebox.showerror("Error", str(e))
+            print(f"Error: {e}")
             return False
 
 
